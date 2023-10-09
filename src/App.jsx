64aa98f8f1910690da82e672563fd5fd4ecb9fd3
Himal2055 <himalkarkee2055@gmail.com>
2023-10-09 13:20:16 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h3 style={{color:"red"}}>Karki k xa</h3>
+      <h5>Age : {11+15}</h5>
+    </>
+  
+  )
+}
+
+export default App
